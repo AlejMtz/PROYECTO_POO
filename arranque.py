2a2d2976664bc5a3ff609daad8c8ac3731c9af23
Samara954 <samaragomezsanchez789@gmail.com>
2024-02-07 18:40:08 -0600

    proveedor

--- arranque.py
+++ arranque.py
@@ -27,6 +27,14 @@ def g_producto():
 def n_cliente():
  return render_template('n_cliente.html')
 
+@app.route('/g_cliente')
+def g_cliente():
+ return render_template('g_cliente.html')
+
+@app.route('/n_proveedor')
+def n_proveedor():
+ return render_template('n_proveedor.html')
+
 #ACTIVAR LA VALIDACION PARA QUE LA PAGINA SE QUEDE EJECUTANDO EN EL SERVIDOR
 if __name__ == '__main__':
     app.run(debug=True)
