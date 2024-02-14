b7d1912b231398d4a559ce299b3c9f82646932d2
Samara954 <samaragomezsanchez789@gmail.com>
2024-02-13 23:49:48 -0600

    guardar datos

--- arranque.py
+++ arranque.py
@@ -1,19 +1,17 @@
 #importar la biblioteca de flask
 #from flask import Flask
-from flask import Flask,render_template
+from flask import Flask,render_template, request
 #render_template me permite retornar archivos html
 
 #llamar al archivo principal desde una funcion
 app = Flask(__name__)#nos permite crear las rutas para las diferentes paginas del sitio web
+
+inventario = []
+
 @app.route('/')
 def inicio():
     #return 'Hola Mundo desde UPVT'
     return render_template('index.html')
-#crear otra ruta
-@app.route('/acercade')
-def acercade():
-  #return 'Muestra informacion acerca del proyecto'
- return render_template('acerca.html')
 
 @app.route('/n_producto')
 def n_producto():
@@ -50,6 +48,25 @@ def n_usuario():
 @app.route('/g_usuario')
 def g_usuario():
  return render_template('g_usuario.html')
+
+@app.route('/n_producto', methods=['POST'])
+def procesar_producto():
+    nombre = request.form['nombre']
+    cantidad = int(request.form['cantidad'])
+    precio = int(request.form['precio'])
+    descripcion = request.form['descripcion']
+    proveedor = request.form['proveedor']
+
+    inventario.append({
+        'Nombre': nombre,
+        'cantidad': cantidad,
+        'precio': precio,
+        'descripcion': descripcion,
+        'proveedor': proveedor
+    })
+
+    return render_template('n_producto.html', inventario=inventario)
+
 
 #ACTIVAR LA VALIDACION PARA QUE LA PAGINA SE QUEDE EJECUTANDO EN EL SERVIDOR
 if __name__ == '__main__':
